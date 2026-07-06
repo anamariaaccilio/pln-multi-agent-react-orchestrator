103306bc9616b0e995e2cb99522804353da362da
Andrea-Coa <andrea.coa@utec.edu.pe>
2026-07-06 08:51:22 -0500

    +naive rag

--- src/pipeline/__init__.py
+++ src/pipeline/__init__.py
@@ -1,5 +1,6 @@
-"""Pipeline end-to-end del módulo de orquestación: multi_agent_rag() y conversión a formato de evaluación."""
+"""Pipeline end-to-end del módulo de orquestación: multi_agent_rag(), naive_rag() y conversión a formato de evaluación."""
 from src.pipeline.eval_format import convert_to_eval_format
 from src.pipeline.multi_agent_rag import multi_agent_rag
+from src.pipeline.naive_rag import naive_rag
 
-__all__ = ["multi_agent_rag", "convert_to_eval_format"]
+__all__ = ["multi_agent_rag", "naive_rag", "convert_to_eval_format"]
